0c2b7c480a8009f7a824b6c558dac99893ae4849
Tanaka Seigo <seigo@1ft-seabass.jp>
2018-03-11 17:47:51 +0900

    05DigitalLedのソースコード更新

---
+++ examples/05DigitalLed/05DigitalLed.ino
@@ -0,0 +1,20 @@
+// LEDを光らせるアナログピン番号
+#define LED_PIN A1
+// 待ち時間（ミリ秒） 1000ミリ秒 = 1秒
+#define DELAY_TIME 1000
+
+void setup() {
+  // LEDを光らせるピンを出力に指定
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // LEDを光らせるピンにデジタル出力の HIGH = ON を指示
+  digitalWrite(LED_PIN, HIGH);
+  // DELAY_TIME の時間だけ待つ
+  delay(DELAY_TIME);
+  // LEDを光らせるピンにデジタル出力の LOW = OFF を指示
+  digitalWrite(LED_PIN, LOW);
+  // DELAY_TIME の時間だけ待つ
+  delay(DELAY_TIME);
+}
