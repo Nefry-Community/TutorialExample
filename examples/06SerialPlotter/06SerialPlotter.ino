5f1433703d6c318f6b785b71df6c8085947d68f2
Tanaka Seigo <seigo@1ft-seabass.jp>
2018-03-11 23:48:17 +0900

    06SerialPlotter更新

---
+++ examples/06SerialPlotter/06SerialPlotter.ino
@@ -0,0 +1,27 @@
+#include <Nefry.h>
+
+void setup() {
+  // SW有効化
+  Nefry.enableSW();
+  // LEDが白に光る（起動時の光り方）
+  Nefry.setLed(255, 255, 255);
+}
+ 
+void loop() {
+  // SWを押した時を判定する Nefry.readSW
+  if (Nefry.readSW()) {
+    // LEDが赤に光る
+    Nefry.setLed(255, 0, 0);
+    // ボタンを押した時は1という値を知らせる
+    Serial.println(10);
+    // 押されたら1秒待つ
+    Nefry.ndelay(1000);
+    // 青に戻る
+    Nefry.setLed(0, 0, 255);
+    // ボタンを離したら0という値を知らせる
+    // Serial.println(0);
+  }
+
+  // 通常時は0を知らせ続ける
+  Serial.println(0);
+}
