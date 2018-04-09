da2014317214c053a339ef52d8976536d7e16718
Tanaka Seigo <seigo@1ft-seabass.jp>
2018-04-09 21:38:44 +0900

    DefaultLed.ino サンプルの挙げ忘れ

---
+++ examples/DefaultLed/DefaultLed.ino
@@ -0,0 +1,25 @@
+#include <Nefry.h>
+
+void setup() {
+  
+}
+
+// フルカラーLEDの色要素（赤 red,緑 green,青 blue）をいれる値の箱（変数）をつくる
+int red,green,blue;
+
+void loop() {
+  // random関数は0-255の数値をランダムに返します
+  
+  // フルカラーLEDの赤要素をランダムに指定
+  red = random(255);
+  // フルカラーLEDの緑要素をランダムに指定
+  green = random(255);
+  // フルカラーLEDの青要素をランダムに指定
+  blue = random(255);
+  
+  // LEDがランダムに点灯します
+  Nefry.setLed(red,green,blue);
+
+  // 1秒待つ
+  Nefry.ndelay(1000);
+}
