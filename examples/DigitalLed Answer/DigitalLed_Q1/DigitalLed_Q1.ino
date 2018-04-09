457ecd04b039f34b7ea6e45a315a8243fbabfce4
Tanaka Seigo <seigo@1ft-seabass.jp>
2018-04-10 00:29:48 +0900

    DigitalLedの課題対応

--- examples/DigitalLed Answer/DigitalLed_Q1/DigitalLed_Q1.ino
+++ examples/DigitalLed Answer/DigitalLed_Q1/DigitalLed_Q1.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+// LEDを光らせるアナログピン番号
+#define LED_PIN A1
 
+void setup() {
+  // LEDを光らせるピンを出力に指定
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // LEDを光らせるピンにデジタル出力の HIGH = ON を指示
+  digitalWrite(LED_PIN, HIGH);
+  // ランダム
+  delay(200 + random(800));
+  // LEDを光らせるピンにデジタル出力の LOW = OFF を指示
+  digitalWrite(LED_PIN, LOW);
+  // ランダム
+  delay(200 + random(800));
 }
