4d09a993ba6fd9742279950b2f54c87539fca020
Tanaka Seigo <seigo@1ft-seabass.jp>
2018-04-09 22:26:31 +0900

    途中経過アップデート

--- examples/DefaultLed Answer/DefaultLed_Q2/DefaultLed_Q2.ino
+++ examples/DefaultLed Answer/DefaultLed_Q2/DefaultLed_Q2.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Nefry.h>
 
+void setup() {
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // 青
+  Nefry.setLed(0,0,255);
+  // 1秒待つ
+  Nefry.ndelay(1000);
+  // 赤
+  Nefry.setLed(255,0,0);
+  // 1秒待つ
+  Nefry.ndelay(1000);
+  // 黄色
+  Nefry.setLed(255,255,0);
+  // 1秒待つ
+  Nefry.ndelay(1000);
 }
--- examples/DefaultSW Answer/DefaultSW_Q2/DefaultSW_Q2.ino
+++ examples/DefaultSW Answer/DefaultSW_Q2/DefaultSW_Q2.ino
@@ -1,9 +1,31 @@
+#include <Nefry.h>
+
+// ON/OFFを管理
+int sw = 0;
+
 void setup() {
-  // put your setup code here, to run once:
-
+  // SW有効化
+  Nefry.enableSW();
+  // LEDが白に光る（起動時の光り方）
+  Nefry.setLed(255, 255, 255);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // SWを押した時を判定する Nefry.readSW
+  if (Nefry.readSW()) {
+    // ON/OFFを管理
+    if( sw == 1 ){
+      // OFF
+      sw = 0;
+      // LEDが赤に光る
+      Nefry.setLed(255, 0, 0);
+    } else {
+      // ON
+      sw = 1;
+      // 青に戻る
+      Nefry.setLed(0, 0, 255);
+    }
+    // 押されたら1秒待つ
+    Nefry.ndelay(1000);
+  }
 }
